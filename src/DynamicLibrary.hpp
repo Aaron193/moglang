2705215355159929eec4f5fdcf4af2f235776e16
Aaron193 <76023256+Aaron193@users.noreply.github.com>
2026-07-11 15:08:19 -0500

    Avoid Windows OPTIONAL macro collision

--- src/DynamicLibrary.hpp
+++ src/DynamicLibrary.hpp
@@ -3,7 +3,12 @@
 #include <string>
 
 #if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <windows.h>
+#ifdef OPTIONAL
+#undef OPTIONAL
+#endif
 #else
 #include <dlfcn.h>
 #endif
