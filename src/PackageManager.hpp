d16c155afecfc03f16d145e8cd928dc2219becc5
Aaron193 <76023256+Aaron193@users.noreply.github.com>
2026-09-04 15:18:45 -0400

    Fix 0.2.0 release metadata

--- src/PackageManager.hpp
+++ src/PackageManager.hpp
@@ -8,7 +8,7 @@
 #include "PackageRegistry.hpp"
 
 #ifndef KELVRA_RUNTIME_VERSION
-#define KELVRA_RUNTIME_VERSION "0.1.7"
+#define KELVRA_RUNTIME_VERSION "0.2.0"
 #endif
 
 struct ProjectRegistryConfig {
